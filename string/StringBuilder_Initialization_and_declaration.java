544b78ec7cf95586ac862c6c6d6c3174fcdcc5be
abhirajsingh524 <thakurarpitsingh524@gmail.com>
2026-09-25 12:05:03 +0530

    add the string folder and its learning

---
+++ string/StringBuilder_Initialization_and_declaration.java
@@ -0,0 +1,11 @@
+class  Build{
+    public static void main(String[] args){
+        StringBuilder sb =new StringBuilder("He is a good boy.");
+        System.out.println("initial stringBuilder:"
+        + sb);
+        sb.append("he is also be a creative mindset");
+        System.out.println("after append "+sb);
+
+    }
+
+}
